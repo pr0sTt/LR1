92d9850209ba9d2430ecfb0edea9d4669cea23eb
pr0sTt <145268699+pr0sTt@users.noreply.github.com>
2024-09-22 18:08:01 +0300

    Add files via upload

---
+++ HelloWorldDemo.java
@@ -0,0 +1,7 @@
+public class HelloWorldDemo {
+    public static void main(String[] argv) {
+        System.out.println("Hello world");
+        System.out.println("Захар Побийвовк, ПЗ-21-1/9");
+        System.out.println("05.09.2024");
+    }
+}
